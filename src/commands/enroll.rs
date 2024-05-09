50c10891b0de0c7e12c4f48bfac5b05d20a34d66
Caden <caden@caden64.com>
2024-05-08 22:59:38 -0600

    removes old role and add new role and nickname properly

--- src/commands/enroll.rs
+++ src/commands/enroll.rs
@@ -1,10 +1,12 @@
+use log::error;
+use poise::serenity_prelude::EditMember;
 use crate::{Context, Error};
 use crate::utils::college_autocomplete::college_autocomplete;
 use crate::checks::remove_role::remove_role;
 use crate::utils::config::REMOVE_ROLE_ID;
 
 #[poise::command(
-slash_command, ephemeral,
+slash_command, guild_only,
 check = "remove_role"
 )]
 pub async fn enroll(
@@ -21,12 +23,17 @@ pub async fn enroll(
         ctx.reply("Unknown university selected please try again").await?;
         return Ok(()) 
     }
-    let member = ctx.author_member().await;
-    if member.is_some() {
-        let member  = member.unwrap();
-        member.add_role(ctx.http(), *ctx.data().config_data.roles.public.get(&university).unwrap()).await?;
-        member.remove_role(ctx.http(), *ctx.data().config_data.roles.private.get(REMOVE_ROLE_ID).unwrap()).await?;
+    let uni_role = ctx.data().config_data.roles.public.get(&university);
+    if uni_role.is_none() {
+        ctx.reply("INVALID UNIVERSITY").await?;
+        return Ok(())
     }
+    let uni_role = *uni_role.unwrap();
+    
+    let builder = EditMember::new().roles(vec![uni_role]).nickname(name.clone());
+    if let Err(e) = ctx.guild_id().unwrap().edit_member(&ctx.http(), ctx.author().id, builder).await.unwrap().remove_role(&ctx.http(), *ctx.data().config_data.roles.private.get(REMOVE_ROLE_ID).unwrap()).await {
+        error!("ERROR REMOVING ROLE {:?}", e)
+    };
     ctx.reply(format!("You have registered as:\nName: {}\nEmail: {}\nInterests: {}\nUniversity: {}\nAdd to Email Distro: {}", name, email, interests, university, email_distro)).await?;
     Ok(())
 }
