5acdc8b49be4ff9b25434525bbdc6e1b9d8a2044
Caden <caden@caden64.com>
2024-05-08 20:10:30 -0600

    added docker and ready notice

--- src/utils/event_handler.rs
+++ src/utils/event_handler.rs
@@ -1,4 +1,4 @@
-use poise::serenity_prelude::FullEvent::GuildMemberAddition;
+use poise::serenity_prelude::FullEvent::{GuildMemberAddition, Ready};
 use poise::serenity_prelude as serenity;
 use crate::{Data, Error, REMOVE_ROLE_ID};
 
@@ -8,11 +8,17 @@ pub async fn event_handler(
     framework: poise::FrameworkContext<'_, Data, Error>,
     _: &Data,
 ) -> Result<(), Error>{
-    if let GuildMemberAddition { new_member, .. } = event {
-        println!("{} Joined Server", new_member.user.name);
-        let default_role_id = *framework.user_data.config_data.roles.private.get(REMOVE_ROLE_ID).expect("UNABLE TO GET REMOVE ROLE ID");
-        new_member.add_role(&ctx.http, default_role_id).await?;
-        println!("{} got default role", new_member.user.name)
-    };
+    match event {
+        Ready { data_about_bot, .. } => {
+            println!("Logged in as {}", data_about_bot.user.name);
+        },
+        GuildMemberAddition { new_member, .. } => {
+            println!("{} Joined Server", new_member.user.name);
+            let default_role_id = *framework.user_data.config_data.roles.private.get(REMOVE_ROLE_ID).expect("UNABLE TO GET REMOVE ROLE ID");
+            new_member.add_role(&ctx.http, default_role_id).await?;
+            println!("{} got default role", new_member.user.name)
+        },
+        _ => {}
+    }
     Ok(())
 }
