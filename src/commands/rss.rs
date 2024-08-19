b00df8b6f2adfe4a908450143043b79dfdc39f4d
caden <caden@caden64.com>
2024-08-18 21:50:17 -0600

    Refactor RSS command error handling and scheduling

--- src/commands/rss.rs
+++ src/commands/rss.rs
@@ -2,19 +2,23 @@ use crate::utils::config::Feed;
 use crate::{Context, Error};
 use chrono::DateTime;
 use chrono_tz::America::Denver;
+use clokwerk::{AsyncScheduler, TimeUnits};
 use poise::serenity_prelude::{CacheHttp, Channel, ChannelId, Http};
+use rss::Channel as rssChannel;
 use std::collections::HashMap;
 use std::error::Error as E2;
 use std::sync::Arc;
 use std::time::Duration;
-use clokwerk::{AsyncScheduler, TimeUnits};
-use rss::Channel as C2;
 
 #[poise::command(slash_command, guild_only, ephemeral)]
 pub async fn rss(ctx: Context<'_>, channel: Channel) -> Result<(), Error> {
-
     let http = &ctx.serenity_context().http;
-    run_one_hour(Arc::clone(http), &ctx.data().config_data.rss.feed, channel.id().get()).await;
+    run_one_hour(
+        Arc::clone(http),
+        &ctx.data().config_data.rss.feed,
+        channel.id().get(),
+    )
+    .await;
     ctx.say(format!("done {}", channel.id().get())).await?;
     Ok(())
 }
@@ -27,12 +31,17 @@ async fn run_one_hour(
     println!("Going to run every hour");
     let mut sched = AsyncScheduler::with_tz(Denver);
     let http = Arc::clone(&http);
-    let feed_urls = urls.values().map(|x| x.url.clone()).collect::<Vec<String>>();
-    sched.every(1.hour()).run(move || send_news(http.clone(), channel_id, feed_urls.clone()));
+    let feed_urls = urls
+        .values()
+        .map(|x| x.url.clone())
+        .collect::<Vec<String>>();
+    sched
+        .every(1.hour())
+        .run(move || send_news(http.clone(), channel_id, feed_urls.clone()));
     tokio::spawn(async move {
         loop {
             sched.run_pending().await;
-            tokio::time::sleep(Duration::from_secs(3500)).await;
+            tokio::time::sleep(Duration::from_secs(3605)).await;
             println!("I have have been falling! - Loki");
         }
     })
@@ -43,16 +52,17 @@ async fn send_news(cache_http: impl CacheHttp, channel: u64, feed_urls: Vec<Stri
     for feed in feed_urls {
         if let Ok(rss_channel) = specific_feed(&feed).await {
             for item in rss_channel.items {
-                if let Some(rfc2822_date) = item.pub_date {
-                    if let Ok(good_date) = parse_and_compare_date(rfc2822_date.as_str()).await {
-                        if good_date {
-                            if let Some(link) = item.link {
-                                if let Some(title) = item.title {
-                                    let send_message = discord_channel.say(cache_http.http(), format!("Article: title: {}, link: {}", title, link)).await;
-                                    if let Err(message_error) = send_message {
-                                       println!("RAN INTO ERROR: {}", message_error) 
-                                    }
-                                }
+                if good_date(item.pub_date).await {
+                    if let Some(link) = item.link {
+                        if let Some(title) = item.title {
+                            let send_message = discord_channel
+                                .say(
+                                    cache_http.http(),
+                                    format!("Article: title: {}, link: {}", title, link),
+                                )
+                                .await;
+                            if let Err(message_error) = send_message {
+                                println!("RAN INTO ERROR: {}", message_error)
                             }
                         }
                     }
@@ -60,6 +70,17 @@ async fn send_news(cache_http: impl CacheHttp, channel: u64, feed_urls: Vec<Stri
             }
         }
     }
+}
+
+async fn good_date(date: Option<String>) -> bool {
+    if let Some(rfc2822_date) = date {
+        if let Ok(good_date) = parse_and_compare_date(rfc2822_date.as_str()).await {
+            if good_date {
+                return true;
+            }
+        }
+    }
+    false
 }
 
 async fn parse_and_compare_date(rfc2822_date: &str) -> Result<bool, Box<dyn E2 + Send>> {
@@ -78,7 +99,7 @@ async fn parse_and_compare_date(rfc2822_date: &str) -> Result<bool, Box<dyn E2 +
     }
     Ok(false)
 }
-async fn specific_feed(url: &String) -> Result<C2, Box<dyn E2 + Send>> {
+async fn specific_feed(url: &String) -> Result<rssChannel, Box<dyn E2 + Send>> {
     let response = match reqwest::get(url).await {
         Ok(res) => res,
         Err(e) => return Err(Box::new(e)),
@@ -89,7 +110,7 @@ async fn specific_feed(url: &String) -> Result<C2, Box<dyn E2 + Send>> {
         Err(e) => return Err(Box::new(e)),
     };
 
-    let channel = match C2::read_from(&content[..]) {
+    let channel = match rssChannel::read_from(&content[..]) {
         Ok(ch) => ch,
         Err(e) => return Err(Box::new(e)),
     };
