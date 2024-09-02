5a96c1fcd167c4a8ecf1a611ac9b7051c2e111a6
caden <caden@caden64.com>
2024-09-01 22:17:32 -0600

    gotta love enums not working how I want them to

--- src/commands/test_enum.rs
+++ src/commands/test_enum.rs
@@ -1,12 +1,10 @@
 use crate::{Context, Error};
-use crate::roles::Roles;
 
 #[poise::command(slash_command)]
 pub async fn inline_choice(
     ctx: Context<'_>,
-    role: Roles
 ) -> Result<(), Error> {
-    ctx.say(format!("selected {} Wow", role))
+    ctx.say("wow")
         .await?;
 
     Ok(())
